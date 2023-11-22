d1c7bf374836b357bc3c33f9193d1d7540785dc4
Alex Lantz <aclantz321@gmail.com>
2023-11-21 19:41:57 -0500

    update add list item, and add cross-out, delete, sort

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
-let inputVal = $('#input').val();
-let button = $('#button');
-let list = $('#list');
-let li = $('<li></li>');
-
-li.append(inputVal);
-
-
-button.on('click', function (e) {
-    if (inputVal === undefined) {
+function newItem () {
+    let inputVal = $('#input').val();
+    let list = $('#list');
+    let li = $('<li></li>');
+    
+        //1. adding new list item
+    li.append(inputVal);
+        
+    if (inputVal === '') {
         console.log('Please write something.')
     }else {
         list.append(li);
     }
-})
 
-    // cross out section, not ready yet
-// function crossOut() {
-//     li.toggleClass('strike');
-// }
-// li,on('dblclick', crossOut);
+        //2. crossing out an item 
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+    li.on('dblclick', function(e) {
+        crossOut();
+    });
 
-    //pointless function, just want to make the errors stop since its defined in the HTML
-function newItem() {
-    console.log(inputVal);
+        //3. Add a delete button
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteItem)
+    function deleteItem() {
+        li.addClass('delete');
+    }
+
+        //4. change order of items
+    list.sortable()
 }
-
 

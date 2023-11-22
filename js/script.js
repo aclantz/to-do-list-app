bad8436f8b344c2cb6063c0afe691debbb47a855
Alex Lantz <aclantz321@gmail.com>
2023-11-21 19:04:56 -0500

    add jquery list item attempt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-let input = $('#input')
+let inputVal = $('#input').val();
+let button = $('#button');
+let list = $('#list');
+let li = $('<li></li>');
+
+li.append(inputVal);
+
+
+button.on('click', function (e) {
+    if (inputVal === undefined) {
+        console.log('Please write something.')
+    }else {
+        list.append(li);
+    }
+})
+
+    // cross out section, not ready yet
+// function crossOut() {
+//     li.toggleClass('strike');
+// }
+// li,on('dblclick', crossOut);
+
+    //pointless function, just want to make the errors stop since its defined in the HTML
+function newItem() {
+    console.log(inputVal);
+}
+
+
